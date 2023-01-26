4bf019befc55d2e752d795c33f12d2912b164ca6
Cogin <cevinmontgomery05@gmail.com>
2023-01-26 11:54:24 -0600

    Finished and Fixed.

--- FindTagContents.java
+++ FindTagContents.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class FindTagContents {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a HTML:");
+        String html = scanner.nextLine();
+        System.out.println("Enter a tag:");
+        String tag = scanner.nextLine();
+
+        int start = html.indexOf("<" + tag + ">");
+        int end = html.indexOf("</" + tag + ">");
+
+        if (start == -1 || end == -1) {
+            System.out.println("No " + tag + " tag found.");
+        } else {
+            System.out.println(html.substring(start + tag.length() + 2, end));
+            
+        }
+        scanner.close();
     }
 }
--- FindWord.java
+++ FindWord.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class FindWord {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a sentence:");
+        String sentence = scanner.nextLine();
+
+        System.out.println("Enter a letter:");
+        String letter = scanner.nextLine();
+
+        int locator = sentence.indexOf(letter);
+
+        if (locator == -1) { System.out.println("No '" + letter + "' found.");}
+        else {
+            int end = sentence.indexOf(" ", locator);
+            if (end == -1) {end = sentence.length();}
+            int start = sentence.lastIndexOf(" ", end - 1) + 1;
+
+            System.out.println(sentence.substring(start, end));
+            scanner.close();
+        }
     }
 }
